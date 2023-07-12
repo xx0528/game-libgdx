bbe6e64f28c22d0114250b4661f78f3ccf0b8131
xx <xx@gmail.com>
2023-07-12 13:19:53 +0800

    版本问题

--- android/src/main/kotlin/com/audit/pass/app/activity/MainActivity.kt
+++ android/src/main/kotlin/com/audit/pass/app/activity/MainActivity.kt
@@ -3,31 +3,38 @@ package com.audit.pass.app.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 
-
-class MainActivity : ComponentActivity(){
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 //        val gameScreen = Intent(this, AndroidLauncher::class.java)
 //        startActivity(gameScreen)
-
         setContent {
-            MainContent()
+                // A surface container using the 'background' color from the theme
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                Greeting("Android")
+            }
         }
-
     }
 }
 
 @Composable
-fun MainContent() {
-    Box(modifier = Modifier.fillMaxSize().background(Color.Blue)) {}
-
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 }
-
