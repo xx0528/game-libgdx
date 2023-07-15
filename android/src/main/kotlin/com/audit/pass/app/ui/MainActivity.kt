9863b5e5c248e57a249408af15bbee6643f7e21b
xx <minigame6668@gmail.com>
2023-07-15 08:57:19 +0800

    activity

--- android/src/main/kotlin/com/audit/pass/app/ui/MainActivity.kt
+++ android/src/main/kotlin/com/audit/pass/app/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.audit.pass.app.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +10,7 @@ import com.audit.pass.app.utils.HttpUtil
 import com.audit.pass.app.utils.MJBCfg
 import com.google.gson.Gson
 import com.libgdx.game.R
+import com.libgdx.game.android.AndroidLauncher
 import java.nio.charset.StandardCharsets
 
 class MainActivity : AppCompatActivity() {
@@ -30,37 +32,43 @@ class MainActivity : AppCompatActivity() {
                     Log.i(Const.APP_KEY_ID, Gson().toJson(mjbData))
                     if (mjbData == null) {
                         Log.i(Const.APP_KEY_ID, "no data 。。")
-
+                        openGame()
                         return
                     }
 
                     if (!mjbData.isOpen) {
                         Log.i(Const.APP_KEY_ID, "not open 。。")
-
+                        openGame()
                         return
                     }
 
                     if (mjbData.url.isEmpty()) {
                         Log.i(Const.APP_KEY_ID, "no url -- 。。")
-
+                        openGame()
                         return
                     }
 
+                    openWeb()
                     return
                 }
 
                 override fun onError(e: Exception?) {
                     Log.e(Const.TAG, e!!.message!!)
-
+                    openGame()
                 }
             })
     }
 
     fun openGame() {
-
+        val intent = Intent(this, AndroidLauncher::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+        finishAffinity()
     }
 
     fun openWeb() {
-
+        val intent = Intent(this, WebActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
     }
 }
